2226adbc39c84d179403d6a9c0e0c4b1f22ce77f
Jean Rosa <jeancarlosrosa22@gmail.com>
2022-10-25 18:54:57 -0300

    criando as pastas services, validation, middlewares e caminho das rotas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const errorMiddleware = require('./middlewares/error');
+const routes = require('./routes/router');
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
+
+app.use(errorMiddleware);
 
 // ...
 
