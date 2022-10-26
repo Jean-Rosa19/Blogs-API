63d4885988e8b1b21bfe4e0cf60c03f239b3428b
Jean Rosa <jeancarlosrosa22@gmail.com>
2022-10-26 16:11:29 -0300

    criando função e rota para buscar usuários

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,23 +6,25 @@ const doesEmailAlreadyExists = async (email) => {
   const result = await User.findOne({ where: { email } });
   return result;
 };
-
 const createNewUser = async ({ displayName, email, password, image }) => {
   const informations = { displayName, email, password, image };
-
   const error = validateNewUser(informations);
   if (error.type) return { type: error.type, message: error.message };
-
   const emailValidation = await doesEmailAlreadyExists(email);
   if (emailValidation) {
     return { type: 'ALREADY_EXISTS', message: 'User already registered' };
   }
-
   await User.create(informations);
   const token = createToken(informations);
   return { type: null, message: token };
 };
 
+const getAllUsers = async () => {
+  const users = await User.findAll({ attributes: { exclude: 'password' } });
+  return users;
+};
+
 module.exports = {
   createNewUser,
+  getAllUsers,
 };
