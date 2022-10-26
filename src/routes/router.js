6842f68bed7e5eb84dca9041c6bcbe797e8496a2
Jean Rosa <jeancarlosrosa22@gmail.com>
2022-10-26 18:41:35 -0300

    criação da rota de categorias

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth.middlewares');
 
 const authRouter = require('./auth.router');
 const userRouter = require('./user.router');
+const categoriesRouter = require('./categories.router');
 
 const routers = express.Router();
 
 routers.use('/login', authRouter);
 
 routers.use('/user', userRouter);
+
+routers.use('/categories', authMiddleware.validateToken, categoriesRouter);
 
 module.exports = routers;
