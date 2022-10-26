e89feee080b03ee96688744ab70a5fc34362e904
Jean Rosa <jeancarlosrosa22@gmail.com>
2022-10-26 10:47:36 -0300

    rota para cadastrar usuario requisito 4

--- src/services/validations/schema.js
+++ src/services/validations/schema.js
@@ -4,7 +4,6 @@ const stringSchema = Joi.string().required().messages({
   'any.required': 'Some required fields are missing',
   'string.empty': 'Some required fields are missing',
 });
-
 const loginSchema = Joi.object({
   email: stringSchema,
   password: stringSchema,
@@ -12,6 +11,14 @@ const loginSchema = Joi.object({
   'object.unknown': 'Invalid fields',
 });
 
+const newUserSchema = Joi.object({
+  displayName: Joi.string().min(8).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  image: Joi.string(),
+});
+
 module.exports = {
   loginSchema,
+  newUserSchema,
 };
--- src/utils/errorMap.js
+++ src/utils/errorMap.js
@@ -1,12 +1,13 @@
 const errorMap = {
-    INVALID_TOKEN: 401,
-    INVALID_FIELDS: 400,
-    MISSING_FIELDS: 400,
-  };
-  
-  const mapError = (type) => errorMap[type] || 500;
-  
-  module.exports = {
-    errorMap,
-    mapError,
-  };
+  ALREADY_EXISTS: 409,
+  INVALID_TOKEN: 401,
+  INVALID_FIELDS: 400,
+  INVALID_VALUES: 400,
+  MISSING_FIELDS: 400,
+};
+
+const mapError = (type) => errorMap[type] || 500;
+module.exports = {
+  errorMap,
+  mapError,
+};
